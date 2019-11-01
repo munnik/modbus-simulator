8dc1120c9d3924edb2bdcf2dcc0fb64f7d9b7d4c
munnik <6munshoe>
2019-11-01 09:56:21 +0100

    Created a dynamic modbus slave, registers can be configured using lambda functions.

---
+++ modbus_dynamic_slave_context.py
@@ -0,0 +1,53 @@
+import datetime
+from typing import Iterable
+
+from pymodbus.interfaces import IModbusSlaveContext
+
+
+class ModbusDynamicSlaveContext(IModbusSlaveContext):
+    def __init__(self):
+        self.store = None
+        self.zero_mode = False
+        self.create_empty_stores()
+
+    def create_empty_stores(self):
+        self.store = {'d': {}, 'c': {}, 'i': {}, 'h': {}}
+
+    def reset(self):
+        self.create_empty_stores()
+
+    def validate(self, fx, address, count=1):
+        if not self.zero_mode:
+            address = address + 1
+
+        for i in range(address, address + count):
+            if i not in self.store[self.decode(fx)]:
+                return False
+        return True
+
+    def getValues(self, fx, address, count=1):
+        if not self.validate(fx, address, count):
+            raise IndexError("One or more addresses are not available")
+
+        if not self.zero_mode:
+            address = address + 1
+
+        result = []
+        for i in range(address, address + count):
+            result.append(self.store[self.decode(fx)][i](datetime.datetime.now()))
+        return result
+
+    def setValues(self, fx, address, values):
+        raise NotImplementedError("Values cannot be set on a ModbusDynamicSlaveContext")
+
+    def setLambda(self, fx, address, lambda_functions):
+        if not self.zero_mode:
+            address = address + 1
+
+        if not isinstance(lambda_functions, Iterable):
+            lambda_functions = [lambda_functions]
+
+        for i, f in zip(range(address, address + len(lambda_functions)), lambda_functions):
+            self.store[self.decode(fx)][i] = f
+
+        return self
